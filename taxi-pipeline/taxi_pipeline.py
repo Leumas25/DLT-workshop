e05b9f6cdacd67722db6814d1ed6ebe41147e086
Leumas25 <35816664+Leumas25@users.noreply.github.com>
2026-03-03 04:50:50 +0000

    First commit

---
+++ taxi-pipeline/taxi_pipeline.py
@@ -0,0 +1,53 @@
+"""NYC taxi data pipeline using dlt REST API source."""
+
+import dlt
+from dlt.sources.rest_api import rest_api_resources
+from dlt.sources.rest_api.typing import RESTAPIConfig
+
+
+@dlt.source
+def taxi_pipeline_rest_api_source():
+    """Define dlt resources from NYC taxi REST API endpoint."""
+    config: RESTAPIConfig = {
+        "client": {
+            "base_url": "https://us-central1-dlthub-analytics.cloudfunctions.net/",
+        },
+        "resource_defaults": {
+            "write_disposition": "replace",
+            "endpoint": {
+                "params": {
+                    "limit": 1000,
+                },
+                "paginator": {
+                    "type": "page_number",
+                    "base_page": 1,
+                    "page_param": "page",
+                    "total_path": None,
+                    "stop_after_empty_page": True,
+                },
+            },
+        },
+        "resources": [
+            {
+                "name": "trips",
+                "endpoint": {
+                    "path": "data_engineering_zoomcamp_api",
+                },
+            },
+        ],
+    }
+
+    yield from rest_api_resources(config)
+
+
+pipeline = dlt.pipeline(
+    pipeline_name="taxi_pipeline",
+    destination="duckdb",
+    refresh="drop_sources",
+    progress="log",
+)
+
+
+if __name__ == "__main__":
+    load_info = pipeline.run(taxi_pipeline_rest_api_source())
+    print(load_info)  # noqa: T201
